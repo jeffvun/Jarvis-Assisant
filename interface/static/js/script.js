cff24e10dfe8ea0ce587d1b8c7ea80f7dde05ff1
Joseph Vunanga <vunangajoseph@gmail.com>
2024-01-28 16:19:56 +0300

    creating a simple HTML template for the web interface

---
+++ interface/static/js/script.js
@@ -0,0 +1,9 @@
+const sendButton = document.getElementById('button-addon2');
+const answerElement = document.querySelector('.card-text');
+
+sendButton.addEventListener('click', () => {
+    const question = document.querySelector('input[type="text"]').value;
+
+    // Send the question to the AI assistant and get the answer
+    // Display the answer in the answerElement element
+});
