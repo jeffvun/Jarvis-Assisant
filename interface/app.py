92b0da6ac6b2665f6c9eb88f8260c05f9d492392
Joseph Vunanga <vunangajoseph@gmail.com>
2024-01-28 16:09:32 +0300

    creating a simple Flask Web Interface

--- interface/app.py
+++ interface/app.py
@@ -1,1 +1,18 @@
 ## Flask app
+
+from flask import Flask, request, render_template
+from assistant.jarvis import Jarvis
+
+app = Flask(__name__)
+jarvis = Jarvis()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        command = request.form['command']
+        response = jarvis.respond(command)
+        return render_template('index.html', command=command, response=response)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
